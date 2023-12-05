230fabca99d371da8ff93a3a6ac286f55a561602
Tom Turner <tom@twt.me.uk>
2023-12-05 15:43:15 +0000

    added a new ajax helpers command in to allow python to load the details page

--- django_examples/cards_examples/views/list.py
+++ django_examples/cards_examples/views/list.py
@@ -1,7 +1,9 @@
+import random
+
 from cards_examples.models import Company
 from cards_examples.views.base import MainMenu
 from django.views.generic import TemplateView
-from django_menus.menu import MenuItem
+from django_menus.menu import MenuItem, AjaxButtonMenuItem
 
 from cards.card_list import CardList
 
@@ -16,17 +18,27 @@ class ExampleCompanyCardList(DjangoCardList):
 
     def get_details_menu(self, details_object):
         return [MenuItem('cards_examples:hello_modal', menu_display='Hello Modal'),
-                MenuItem('cards_examples:hello_modal', menu_display='Hello Modal2')]
+                MenuItem('cards_examples:hello_modal', menu_display='Hello Modal2'),
+                AjaxButtonMenuItem(button_name='reload',
+                                   menu_display='Reload',
+                                   css_classes='btn btn-secondary')]
 
     def get_details_title(self, details_object):
         return f'Details {details_object.pk}'
 
     def get_details_data(self, card, details_object):
+        number = random.randint(1, 1000)
+
         card.add_rows(['name', 'active'],
                       [{'field': 'company_category__name', 'label': 'Category'}],
                       'importance',
                       {'field': 'importance'},
-                      'get_display_name')
+                      'get_display_name',
+                      {'value': number, 'label': 'Random'})
+
+    # @ConfirmAjaxMethod(message="Would you like to reload the main card")
+    def button_reload(self, *args, **kwargs):
+        return self.command_response('load_details')
 
 
 class ExampleCompanyCardEmptyList(ExampleCompanyCardList):
