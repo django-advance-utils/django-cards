d2dc09c5197005b9b1a801db95cead6cbbbcafe0
Tom Turner <tom@twt.me.uk>
2025-01-15 18:18:13 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="django-cards",
-    version="0.11.7",
+    version="0.11.8",
     author="Thomas Turner",
     description="Django app that allows you make cards",
     long_description=long_description,
